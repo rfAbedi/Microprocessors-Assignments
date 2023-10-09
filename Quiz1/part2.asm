5cbe01181d403b319934a8391bc3dedbef5514fb
Shayan Kebriti <shayankebriti@gmail.com>
2023-10-09 17:28:27 +0330

    Add loop and print

--- Quiz1/part2.asm
+++ Quiz1/part2.asm
@@ -1,12 +1,28 @@
-DATA1 DB 0
+.MODEL SMALL
+.STACK 64
+.DATA
+    DATA1 DB 04H
+    char BYTE ?
+.CODE
 
-CoSeg segmant 
-    MOVE AH, 1
-    INT 21H
-BACK:
-    MOV SA, 0
-    
-    LOOP BACK
-    MOV AH, 4CH
-    INT 21H
-CoSeg ends
+MAIN    PROC FAR
+        MOV AX,@DATA
+        MOV DS,AX
+        MOV BL, DATA1
+        MOV AH, 01H
+        INT 21H ; WAITS FOR USER CLICKING
+        MOV char, AL
+    BACK:
+        CMP BL, 00H
+        JE FINISH
+        MOV DL, char
+        MOV AH, 02H
+        INT 21H
+        DEC BL
+        LOOP BACK
+    FINISH:
+		MOV AH, 4CH
+        INT 21H
+MAIN    ENDP
+
+END MAIN
