8aed243941b4f8802b1c4541cf12551708f787c9
Erfun-ABD <erfuun.abd@gmail.com>
2023-10-22 02:05:23 +0330

    Add Lab2

---
+++ Lab2-3/Lab2/Main.asm
@@ -0,0 +1,160 @@
+.MODEL SMALL
+.STACK 64
+.DATA
+    ROM DW 1100000000000000B
+    RAM DW 1100010000000000B
+    KPMN DB ?
+
+    M DB ?
+    N DB ?
+
+    r DB 01H
+    x_fact DB ? ; IS USED TO CALCULATE factorial of x
+    n_fact DW ?
+    r_fact DW ?
+    n_r_fact DW ?
+    comb DB ? ; ANSWER OF Combination
+    OUTER_LOOP_COUNT_TEMP DW ?
+.CODE  
+
+MAIN    PROC FAR
+
+    CALL KPEXT
+
+ENDLESS:
+	JMP ENDLESS
+MAIN    ENDP
+
+
+
+KPEXT    PROC
+READ_ROM:
+	MOV AX, @DATA
+	MOV DS, AX
+	
+	MOV AX, ROM
+	MOV DS, AX
+        
+	MOV DI, 00H
+	MOV CL, [DI] ; READ M
+    MOV CH, [DI+1] ; READ N
+
+SAVE_DATA:
+    MOV AX, @DATA
+	MOV DS, AX
+
+    DEC CL
+    MOV M, CL
+    DEC CH
+    MOV N, CH
+
+WRITE_RAM:
+	MOV AX, RAM
+	MOV DS, AX
+	
+	MOV DI, 00H
+
+calculation:
+    MOV CX, 0000H; COUNTER = 0
+L1:
+    MOV AX, @DATA
+	MOV DS, AX
+
+    CMP CL, M
+    JA L1_END ; FINISH WHEN COUNTER > M
+
+        MOV r, CL ; r = COUNTER
+
+        ; --- M! ---
+        MOV AH, M
+        MOV x_fact, AH ; x = M
+        MOV BL, AH
+        CALL FACT
+        MOV n_fact, AX
+        ; ----------
+
+        ; --- r! ---
+        MOV DL, r
+        MOV x_fact, DL ; x = r
+        CALL FACT
+        MOV r_fact, AX
+        ; ----------
+
+        ; --- (M-r)! ---
+        MOV DL, M ; DL = M
+        SUB DL, r ; DL = M - r
+        MOV x_fact, DL ; x = M - r
+        CALL FACT
+        MOV n_r_fact, AX
+        ; ---------------
+
+        ; --- M!/(r! (M-r)!) ---
+        MOV AX, n_fact
+        SUB DX, DX
+        DIV r_fact
+        SUB DX, DX
+        DIV n_r_fact
+        MOV comb, AL
+        ; ----------------------
+
+
+    SUB BX, BX ; Clear AX
+    MOV BL, comb ; AX = comb
+
+W_RAM:
+    MOV AX, RAM
+    MOV DS, AX
+    
+    MOV DI, CX
+    MOV [DI], BL ; WRITE COMBINATION IN RAM
+
+    INC CX ; COUNTER++
+    INC CX ; COUNTER++
+    LOOP L1 ; COUNTER--
+
+L1_END:
+    SUB BX, BX
+    MOV BL, N
+    MOV DI, BX
+
+    MOV AX, RAM
+    MOV DS, AX
+
+    MOV BL, [DI] ; READ M
+
+    MOV AX, @DATA
+    MOV DS, AX
+
+    MOV KPMN, BL ; KPMN = M
+
+RET
+
+KPEXT    ENDP
+
+
+FACT PROC ; Saves x! IN (DX, AX)
+
+    MOV OUTER_LOOP_COUNT_TEMP, CX
+
+    MOV CX, 01H ; COUNTER = 1
+    MOV AX, 01H ; AX = 1 
+    MOV DX, 0000H ; CLEAR DX
+
+BACK:
+    MOV BH, 00H
+    MOV BL, x_fact
+    CMP CX, BX
+    JA FINISH ; FINISH WHEN COUNTER > x
+    MUL CX ; (DX, AX) = CX * AX
+    INC CX ; COUNTER++
+    INC CX ; COUNTER++
+    LOOP BACK ; COUNTER--
+
+
+FINISH:
+    MOV CX, OUTER_LOOP_COUNT_TEMP
+    RET
+    ; THE ANSWER IS STORED IN (DX, AX)
+    
+FACT ENDP
+    END MAIN
