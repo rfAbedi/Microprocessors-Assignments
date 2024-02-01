50260b8e12eecc7b75a51c21618781fb4a96bd3b
Erfun-ABD <erfuun.abd@gmail.com>
2024-02-01 18:04:10 +0330

    Update Lab 12

--- Lab12/Visual Studio Project/Lab12/Lab12.cpp
+++ Lab12/Visual Studio Project/Lab12/Lab12.cpp
@@ -19,6 +19,34 @@ void print_array(float* arr, int size, bool is_float) {
     _cprintf_s("]\n");
 }
 
+void function_c(float* zi1, float* zi2, float* zo1) {
+    for (int i = 0; i < 50; i++) {
+        zo1[i] = 1 / (2 * zi1[i] * zi2[i]);
+    }
+}
+
+void function_sse(float* zi1, float* zi2, float* zo2) {
+    __asm {
+        mov eax, zi1
+        mov ebx, zi2
+        mov edx, zo2
+        mov ecx, 0
+    LOOP1:
+        movups xmm0, xmmword ptr [eax + 4 * ecx]
+        movups xmm1, xmmword ptr [ebx + 4 * ecx]
+
+        movaps xmm2, xmm0
+        mulps xmm2, xmm1
+        addps xmm2, xmm2
+        rcpps xmm3, xmm2
+        movups xmmword ptr [edx + 4 * ecx], xmm3
+
+        add ecx, 4
+        cmp ecx, 50
+        jl LOOP1
+    }
+}
+
 int main()
 {
     float a, b;
@@ -49,16 +77,15 @@ int main()
     _cprintf_s("\nzi2: ");
     print_array(zi2, 50, true);
 
+
     auto start = high_resolution_clock::now();
 
-    for (int i = 0; i < 50; i++) {
-        zo1[i] = 1 / (2 * zi1[i] * zi2[i]);
-    }
+    function_c(zi1, zi2, zo1);
 
     auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
+    auto duration = duration_cast<nanoseconds>(stop - start);
 
-    _cprintf_s("\nTime taken by function: %f microseconds", duration.count());
+    _cprintf_s("\nTime taken by 'C' function: %d nanoseconds", duration.count());
     _cprintf_s("\nzo1: ");
     print_array(zo1, 50, false);
     _cprintf_s("\n");
@@ -66,30 +93,17 @@ int main()
 
     start = high_resolution_clock::now();
 
-    __asm {
-        mov ecx, 0
-    LOOP1:
-        movups xmm0, oword ptr zi1[4 * ecx]
-        movups xmm1, oword ptr zi2[4 * ecx]
-
-        movups xmm2, xmm0
-        mulps xmm2, xmm1
-        addps xmm2, xmm2
-        rcpps xmm3, xmm2
-        movups oword ptr zo2[4 * ecx], xmm3
-
-        add ecx, 4
-        cmp ecx, 50
-        jl LOOP1
-    }
+    function_sse(zi1, zi2, zo2);
 
     stop = high_resolution_clock::now();
-    duration = duration_cast<microseconds>(stop - start);
+    duration = duration_cast<nanoseconds>(stop - start);
     
-    _cprintf_s("Time taken by function: %f microseconds", duration.count());
+    _cprintf_s("Time taken by 'SSE' function: %d nanoseconds", duration.count());
     _cprintf_s("\nzo2: ");
     print_array(zo2, 50, false);
     _cprintf_s("\n");
+
+    while (true) {}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
